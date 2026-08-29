c9c3042d3651463278c7f0f448ef6829483ffa35
RaphaBonfim <contato@raphabonfim.com.br>
2026-08-28 22:06:05 -0300

    i'm on fire today. constants, entites and dungeon generation all with new homes. I even fixed the indentation on the room_generation function BEFORE I run the script. AND I MADE NO MISTAKE. take that python.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 
 from constants import *
 from entities import Entity, get_blocking_entity
+from game_map import Tile, Room, is_walkable, carve_room, carve_h_corridor, carve_v_corridor, generate_dungeon
 
 # === Standard Game Loop Structure in 5 steps: === #
 
@@ -13,97 +14,11 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("r/roguelikedev does the tutorial 2026")
 clock = pygame.time.Clock()
 
-# === CLASSES === #
-
-class Tile:
-    def __init__(self, color, walkable):
-        self.color = color
-        self.walkable = walkable
-
-class Room:
-    def __init__(self, x1, y1, x2, y2):
-        # normalizes so x1 and y1 are always the smaller
-        # that means they are always the top-left corner
-        self.x1 = min(x1, x2)
-        self.y1 = min(y1, y2)
-        self.x2 = max(x1, x2)
-        self.y2 = max(y1, y2) 
-
-    def center(self):
-        # calculates the center of room position using math 
-        return ((self.x2 - self.x1) // 2 + self.x1, (self.y2 - self.y1) // 2 + self.y1) 
-    
-    def intersects(self, other):
-        if self.x2 < other.x1 or self.x1 > other.x2 or self.y2 < other.y1 or self.y1 > other.y2:
-            return False
-        return True
-
 
 PLAYER_SPRITE = pygame.image.load("./assets/player.png").convert_alpha()
 MONSTER_SPRITE = pygame.image.load("./assets/monster.png").convert_alpha()
 
-# === HELPER FUNCTIONS === #
-
-def is_walkable(x, y, floor_map):
-    if x < 0 or x >= MAP_WIDTH:
-        return False
-    if y < 0 or y >= MAP_HEIGHT:
-        return False
-    return floor_map[x][y].walkable
-
-def carve_room(floor_map, x1, y1, x2, y2): # the +1 makes the room inclusive that means that we INCLUDE the coordinate in the room
-    for x in range(x1, x2 + 1):
-        for y in range(y1, y2 + 1):
-            floor_map[x][y] = Tile(DARK_GRAY, True)
-
-def carve_h_corridor(floor_map, x1, x2, y):
-    for x in range(min(x1, x2), max(x1, x2) + 1):
-        floor_map[x][y] = Tile(DARK_GRAY, True)
-
-def carve_v_corridor(floor_map, y1, y2, x):
-    for y in range(min(y1, y2), max(y1, y2) + 1):
-        floor_map[x][y] = Tile(DARK_GRAY, True)
- 
-
-
-
-# === ROOM GENERATION === # 
-
-# setting all tiles to stone first
-floor_map = [[Tile(LIGHT_GRAY, False) for y in range(MAP_HEIGHT)] for x in range(MAP_WIDTH)]
-
-rooms = []
-
-for _ in range(50):
-    room_valid = True
-
-    # quick maths to ensure rooms are inside boundaries
-    # defining room_w and room_h helps keep things under control
-    # they go inside the loop because I want diff numbers every iteration
-
-    room_width = random.randint(4,6)
-    room_height = random.randint(4,6)
-    x1 = random.randint(1, MAP_WIDTH - room_width - 1)
-    y1 = random.randint(1, MAP_HEIGHT - room_height - 1)
-    x2 = x1 + room_width
-    y2 = y1 + room_height
-
-    r = Room(x1, y1, x2, y2)
-    for other in rooms:
-        if r.intersects(other):
-            room_valid = False
-            break
-    if room_valid:
-        carve_room(floor_map, r.x1, r.y1, r.x2, r.y2)
-
-        # connecting the room to the previous one
-        if rooms:
-            new_room_center_x, new_room_center_y = r.center()
-            old_room_center_x, old_room_center_y = rooms[-1].center()
-            carve_h_corridor(floor_map, new_room_center_x, old_room_center_x, old_room_center_y)
-            carve_v_corridor(floor_map, new_room_center_y, old_room_center_y, new_room_center_x)
-        rooms.append(r)
-
+floor_map, rooms = generate_dungeon(50, 4, 6)
 
 # add entities
 
